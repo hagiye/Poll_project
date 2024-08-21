09f76c005a974f13b85b1c4250a47a0674ec2356
hagiye <hagerneshtadesse@MB-S7219>
2024-08-21 12:13:35 +0300

    choice feature added

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,23 @@
-from django.contrib import admin
+#from django.contrib import admin
 
 # Register your models here.
-from .models import Question
-admin.site.register(Question)
+#from .models import Question
+
+#class QuestionAdmin(admin.ModelAdmin):
+#    fields = ["pub_date", "question_text"]
+
+#admin.site.register(Question, QuestionAdmin)
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
