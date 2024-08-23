0404a1415172f338be7565871e42acc31229f93f
hagiye <hagerneshtadesse@MB-S7219>
2024-08-23 13:31:06 +0300

    filter and search features added

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,27 @@
 import datetime
 from django.db import models
 from django.utils import timezone
-
+from django.contrib import admin
 class Question(models.Model):
+
+    search_fields = ["question_text"]
+    list_filter = ["pub_date"]
+
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.question_text 
     
+    @admin.display(
+            boolean = True,
+            ordering= "pub_date",
+            description= "waspublished recently",
+        
+    )
+
+    
+
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
